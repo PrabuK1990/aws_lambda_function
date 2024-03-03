ef0424a00ac1b3930c3d85336027507bf03b331e
prabu-kumaravel-nh120303 <prabu.kumaravel@hertz.com>
2024-03-04 02:50:41 +0530

    hjhjb

--- test2/lambda_function.py
+++ test2/lambda_function.py
@@ -2,6 +2,7 @@ import json
 
 def lambda_handler(event, context):
     print('aws started')
+    print('hello')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
